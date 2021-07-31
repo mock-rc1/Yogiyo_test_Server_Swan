5d8bbcc455a974897eef8f7dff3fda4346623297
vividswan <vividswan@naver.com>
2021-07-31 18:25:05 +0900

    Init: 프로젝트 생성 및 build.gradle 세팅

---
+++ yogiyo/src/main/java/com/server/yogiyo/YogiyoApplication.java
@@ -0,0 +1,13 @@
+package com.server.yogiyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogiyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogiyoApplication.class, args);
+	}
+
+}
