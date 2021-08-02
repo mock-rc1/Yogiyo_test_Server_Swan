f5de3ba287cd028e2b859f42c0f50277f53acdb9
vividswan <vividswan@naver.com>
2021-08-02 19:41:27 +0900

    Feat: Authentication 기능 추가 (UserDetails, UserDetailsService 구현)

---
+++ yogiyo/src/main/java/com/server/yogiyo/configure/security/authentication/CustomUserDetailService.java
@@ -0,0 +1,27 @@
+package com.server.yogiyo.configure.security.authentication;
+
+import com.server.yogiyo.account.domain.Account;
+import com.server.yogiyo.account.domain.AccountRepository;
+import com.server.yogiyo.configure.response.exception.CustomException;
+import com.server.yogiyo.configure.response.exception.CustomExceptionStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        Optional<Account> optionalAccount = accountRepository.findByEmail(email);
+        if (!optionalAccount.isPresent()) throw new CustomException(CustomExceptionStatus.ACCOUNT_NOT_FOUND);
+        return new CustomUserDetails(optionalAccount.get());
+    }
+
+}
