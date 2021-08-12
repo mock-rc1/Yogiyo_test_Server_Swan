f162f2f1a508f2d0d4d3888d5e4ee9b4383a0f88
vividswan <vividswan@naver.com>
2021-08-12 21:08:53 +0900

    Feat: 네이버 NaverDirection5을 이용한 두 좌표 사이의 최소 거리 API 완성

---
+++ yogiyo/src/main/java/com/server/yogiyo/util/location/NaverDirection5.java
@@ -0,0 +1,42 @@
+package com.server.yogiyo.util.location;
+
+import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.text.DecimalFormat;
+
+@RequiredArgsConstructor
+@Component
+public class NaverDirection5 {
+
+    @Value("${naver.API_KEY_ID}")
+    private String X_NCP_APIGW_API_KEY_ID;
+
+    @Value("${naver.API_KEY}")
+    private String X_NCP_APIGW_API_KEY;
+
+
+    public String getDistance(String x, String y) {
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-NCP-APIGW-API-KEY-ID", X_NCP_APIGW_API_KEY_ID);
+        headers.add("X-NCP-APIGW-API-KEY", X_NCP_APIGW_API_KEY);
+        String body = "";
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        ResponseEntity<String> responseEntity =
+                rest.exchange("https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start="+x+"&goal="+y+"&option=traoptimal", HttpMethod.GET, requestEntity, String.class);
+        HttpStatus httpStatus = responseEntity.getStatusCode();
+        int status = httpStatus.value();
+        JSONObject route = new JSONObject(responseEntity.getBody()).getJSONObject("route");
+        Object traoptimalObj
+                = new JSONObject(route.getJSONArray("traoptimal").get(0).toString()).getJSONObject("summary").get("distance");
+        Double distance = Double.parseDouble(traoptimalObj.toString())/1000.0;
+        String format = new DecimalFormat("#.##").format(distance);
+        return format;
+    }
+
+}
